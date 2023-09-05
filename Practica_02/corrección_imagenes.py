2f6bd07c9a9a8ff735fffc51fcedc18b8a2120c4
Maximiliano <maximilianogalindo7@gmail.com>
2023-09-04 19:05:23 -0600

    Functions

--- Practica_02/corrección_imagenes.py
+++ Practica_02/corrección_imagenes.py
@@ -13,6 +13,41 @@ imagen_modelo = cv2.imread('Ajedrez_Ortogonal.jpg')
 #cv2.waitKey(0)
 #cv2.destroyAllWindows()
 
+
+# Función para hacer la transformación
+
+def warp_perspective(image, H, output_size):
+    
+
+    # Obtener las dimensiones de la imagen de entrada
+    height, width = image.shape[:2] 
+
+    # Crear una matriz para la imagen de salida con tipo de dato np.uint8
+    output_image = np.zeros((output_size[0], output_size[1], 3), dtype=np.uint8)
+
+    # Calcular la inversa de la matriz de transformación
+    H_inv = np.linalg.inv(H)
+
+    for y_out in range(output_size[0]):
+        for x_out in range(output_size[1]):
+            
+            # Mapear las coordenadas (x_out, y_out) de la imagen de salida a la imagen de entrada
+            point_in = np.dot(H_inv, np.array([x_out, y_out, 1]))
+
+            # Normalizar las coordenadas
+            point_in /= point_in[2]
+
+            x_in, y_in = point_in[:2].astype(int)
+
+            # Verificar si las coordenadas mapeadas están dentro de los límites de la imagen de entrada
+            if 0 <= x_in < width and 0 <= y_in < height:
+                output_image[y_out, x_out] = image[y_in, x_in]
+
+    return output_image
+
+
+
+
 # Puntos de referencia de la imagen modelo (Ajedres_Ortogonal.jpg)
 puntos_model = np.array([
     [140, 85],
@@ -61,16 +96,14 @@ print("H = ")
 print(H)
 
 # Aplicar la transformación perspectiva utilizando la matriz H
-imagen_corregida = cv2.warpPerspective(imagen_proyectada, H, (imagen_proyectada.shape[1], imagen_proyectada.shape[0]))
+imagen_corregida = warp_perspective(imagen_proyectada, H, (imagen_modelo.shape[1], imagen_modelo.shape[0]))
 
 # Guardar la imagen ortogonal resultante
 cv2.imwrite('Imagen_corregida.jpg', imagen_corregida)
 
-
-
 alpha = 0.5
 
-imagen_modelo = cv2.resize(imagen_modelo, (imagen_proyectada.shape[1], imagen_proyectada.shape[0]))
+imagen_modelo = cv2.resize(imagen_modelo, (imagen_corregida.shape[1], imagen_corregida.shape[0]))
 
 overlapped_images = cv2.addWeighted(imagen_corregida, alpha, imagen_modelo, 1 - alpha, 0)
 
